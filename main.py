11ae39932b0dfc677db3125acc97ed6a94a6d0d9
CLopMan <100472092@alumnos.uc3m.es>
2021-11-24 11:07:39 +0100

    versión inicial. Código a limpio

---
+++ main.py
@@ -0,0 +1,24 @@
+import pyxel
+
+W = 256
+H = 256
+CAPTION = "MARIO BROSS"
+
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+
+
+
+
+
+
+def draw():
+    pyxel.cls(0)
+
+
+pyxel.init(W, H, caption=CAPTION)
+
+pyxel.load("mario_assets.pyxres")
+
+pyxel.run(update, draw)
