86a2410882df211b741aac27e5986ef6cf774c8d
CLopMan <100472092@alumnos.uc3m.es>
2021-12-01 11:42:36 +0100

    Constantes y corrección de color de fondo de sprites

--- constantes.py
+++ constantes.py
@@ -5,13 +5,27 @@ HEIGHT = 256
 X = 0
 VELOCIDAD = 0
 
+# Enemigos
+sprite_goomba = (0, 0, 48, 16, 16)
+sprite_koopa = (0, 0, 48, 16, 16)
+ENEMIGOS_XY = ((72, 144, "", sprite_goomba),)
 VELOCIDAD_ENEMIGOS = 0.5
 
-
+# Mario
 posicion_inicial_mario = [5, 208]
+velocidad_limite = (3, 6)
+
+
+# Bloques
 posicion_bloques = ((184, 184), (284, 200), (384, 160))
+sprite_bloque = (0, 32, 16, 16, 16)
+sprite_bloqueLiso= (0, 48, 16, 16, 16)
+sprite_bloqueDiamante = (0, 16, 32, 16, 16)
+sprite_tuberia = (0, 0, 80, 26, 31)
+
+# Generales
 GRAVEDAD = 0.6
-ENEMIGOS_XY = ((72, 144, "", (0, 0, 48, 16, 16)),)
+
 
 # hitbox = (16, 16)
 # hitbox_mario_grande = (16, 32)
